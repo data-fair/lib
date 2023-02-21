c1cb13a4f90365c5d40d83469acf87ca97d62938
Alban Mouton <alban.mouton@gmail.com>
2023-02-21 23:18:45 +0100

    feat: work on schemas management

--- src/types/build.ts
+++ src/types/build.ts
@@ -5,17 +5,30 @@
 const { compile } = require('json-schema-to-typescript')
 const fs = require('node:fs')
 const path = require('node:path')
-const { camelCase } = require('camel-case');
+const { camelCase } = require('camel-case')
+const { sessionStateSchema } = require('./session-state');
 
 (async () => {
   const dir = path.resolve(process.argv[2] || './types')
   console.log(`look for schemas in subdirectories of ${dir}`)
 
+  const myResolver = {
+    order: 1,
+    canRead (file: { url: string }) {
+      return file.url.includes('@data-fair/lib/types/')
+    },
+    read (file: { url: string }, callback: (err: Error | null, doc?: any) => void) {
+      const key = file.url.split('@data-fair/lib/types/').pop()
+      if (key === 'session-state') callback(null, sessionStateSchema)
+      else callback(new Error(`unknown schema in @data-fair/lib ${key}`))
+    }
+  }
+
   for (const key of fs.readdirSync(dir)) {
     if (key === 'node_modules' || !fs.lstatSync(path.join(dir, key)).isDirectory()) continue
     console.log(`compute ${key}`)
     const schema = require(path.join(dir, key, 'schema'))
-    const ts = await compile(schema, key)
+    const ts = await compile(schema, key, { $refOptions: { resolve: { '@data-fair/lib': myResolver } } })
     fs.writeFileSync(path.join(dir, key, 'index.ts'), `${ts}
 export const ${camelCase(key)}Schema = ${JSON.stringify(schema, null, 2)}
 `)
