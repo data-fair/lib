3a261480e4ceef532d1155d4603f069fa3ab7655
Alban Mouton <alban.mouton@gmail.com>
2023-03-13 14:12:26 +0100

    fix: broken import in built files

--- src/types/build.ts
+++ src/types/build.ts
@@ -101,7 +101,7 @@ export const schema = ${JSON.stringify(schema, null, 2)}
 // validate function compiled using ajv
 // @ts-ignore
 import validateUnsafe from './validate.js'
-import { validateThrow } from '${inLib ? '../validation' : '@data-fair/lib/types/validation'}'
+import { validateThrow } from '${inLib ? '../validation' : '@data-fair/lib/cjs/types/validation'}'
 import { type ValidateFunction } from 'ajv'
 export const validate = (data: any, lang: string = 'fr', name: string = 'data', internal?: boolean): ${mainTypeName} => {
   return validateThrow<${mainTypeName}>(validateUnsafe as unknown as ValidateFunction, data, lang, name, internal)
