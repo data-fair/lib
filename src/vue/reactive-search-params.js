467769aa4c5189283270d883f147824229ac50f9
Alban Mouton <alban.mouton@gmail.com>
2023-11-22 18:21:05 +0100

    chore: more usable typing of reactiveSearchParams

--- src/vue/reactive-search-params.js
+++ src/vue/reactive-search-params.js
@@ -2,4 +2,7 @@
 // adapted only in none-ssr context
 import { useUrlSearchParams } from '@vueuse/core'
 
+// this typing is not really correct (values can be arrays if repeated in the URL)
+// but it's enough for our use case as we always use single values and URL with repeated values are not generated
+/** @type {Record<string, string>} */
 export default useUrlSearchParams('history')
