4654c3a501531323b7e7a98d54b5715c2da98c40
Nicolas Bonnel <nicolas.bonnel@gmail.com>
2024-03-14 18:21:50 +0100

    feat: date range picker & add v-model to search-address component

--- src/vuetify/search-address.vue
+++ src/vuetify/search-address.vue
@@ -1,41 +1,26 @@
-<template>
-  <v-autocomplete
-    v-model="address"
-    v-model:search="searchAddress"
-    :items="addressesList"
-    :loading="loadingAddresses"
-    no-filter
-    :clearable="true"
-    return-object
-    hide-no-data
-    hide-details
-    placeholder="Saisissez une adresse"
-    variant="solo"
-    density="compact"
-    menu-icon=""
-  />
-</template>
-
 <script setup>
 import { ref, watch } from 'vue'
 import { ofetch } from 'ofetch'
-import { debounceFilter, watchWithFilter } from '@vueuse/core'
+import { useDebounceFn } from '@vueuse/core'
 
-const emit = defineEmits(['goTo'])
+defineProps({
+  variant: { type: String, default: undefined }
+})
+const emit = defineEmits(['selected'])
+const model = defineModel({ type: String })
 
 /** @type {import('vue').Ref<any[]>} */
 const addressesList = ref([])
-const searchAddress = ref('')
 const loadingAddresses = ref(false)
 /** @type {import('vue').Ref<any>} */
 const address = ref(null)
 
-const findAdresses = async () => {
+const findAdresses = useDebounceFn(async (/** @type {string} */search, /** @type {string} */selectedId) => {
   loadingAddresses.value = true
-  if (!searchAddress.value || searchAddress.value.length < 3) {
-    addressesList.value = address.value ? [address] : []
+  if (!search || search.length < 3) {
+    addressesList.value = address.value ? [address.value] : []
   } else {
-    const params = { q: searchAddress.value }
+    const params = { q: search }
     const result = (await ofetch('https://api-adresse.data.gouv.fr/search/', { params }))
     addressesList.value = result.features.map((/** @type {any} */f) => ({
       title: f.properties.label,
@@ -46,20 +31,45 @@ const findAdresses = async () => {
       }
     }))
   }
+  if (selectedId) {
+    address.value = addressesList.value.find(a => a.value.id === selectedId)
+  }
   loadingAddresses.value = false
-}
+}, 300)
 
-watchWithFilter(
-  searchAddress,
-  () => findAdresses(),
-  { eventFilter: debounceFilter(300) }
-)
+if (model.value && model.value.length) {
+  findAdresses(...JSON.parse(`[${model.value}]`))
+}
 
 watch(
   address,
   (addr) => {
-    if (addr) emit('goTo', addr.value)
+    if (addr) {
+      model.value = JSON.stringify([addr.title, addr.value.id]).slice(1, -1)
+    } else {
+      model.value = undefined
+      addressesList.value = []
+    }
+    emit('selected', addr ? addr.value : undefined)
   }
 )
-
 </script>
+
+<template>
+  <v-autocomplete
+    v-model="address"
+    :items="addressesList"
+    :loading="loadingAddresses"
+    no-filter
+    :clearable="true"
+    return-object
+    hide-no-data
+    hide-details
+    label="Adresse"
+    placeholder="Saisissez une adresse"
+    :variant="variant"
+    density="compact"
+    menu-icon=""
+    @update:search="search => findAdresses(search)"
+  />
+</template>
