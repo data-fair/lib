c4be65c327c72c4572b823eef37cfc14e61f740c
Alban Mouton <alban.mouton@gmail.com>
2024-09-06 17:20:37 +0200

    feat: simple eslint config

---
+++ src/eslint/recommended.js
@@ -0,0 +1,18 @@
+const deprecatedModules = [
+  { name: 'event-to-promise', message: 'Please use @data-fair/lib/event-promise.js' },
+  { name: '@koumoul/sd-vue', message: 'Please use @data-fair/lib/vue/session.js' },
+  { name: '@koumoul/sd-express', message: 'Please use @data-fair/lib/express/session.js' },
+  { name: '@data-fair/sd-vue', message: 'Please use @data-fair/lib/vue/session.js' },
+  { name: '@data-fair/sd-express', message: 'Please use @data-fair/lib/express/session.js' },
+  { name: '@data-fair/processings-test-utils', message: 'Please use @data-fair/lib/processings/test-utils.js' },
+  { name: 'rfdc', message: 'Please use @data-fair/lib/clone.js' },
+  { name: 'http-errors', message: 'Please use @data-fair/lib/http-errors.js' }
+]
+
+/** @type {import('eslint').ESLint.ConfigData} */
+export default {
+  rules: {
+    'no-restricted-imports': ['error', ...deprecatedModules],
+    'no-restricted-modules': ['error', ...deprecatedModules]
+  }
+}
