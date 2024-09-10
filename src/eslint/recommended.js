f6a1dc76cd24e57c238501abfc54ff7992d6eb4d
Alban Mouton <alban.mouton@gmail.com>
2024-09-10 16:07:31 +0200

    feat: work on req-origin utility

--- src/eslint/recommended.js
+++ src/eslint/recommended.js
@@ -1,4 +1,4 @@
-const deprecatedModules = [
+export const deprecatedModules = [
   { name: 'event-to-promise', message: 'Please use @data-fair/lib/event-promise.js' },
   { name: '@koumoul/sd-vue', message: 'Please use @data-fair/lib/vue/session.js' },
   { name: '@koumoul/sd-express', message: 'Please use @data-fair/lib/express/session.js' },
@@ -6,7 +6,8 @@ const deprecatedModules = [
   { name: '@data-fair/sd-express', message: 'Please use @data-fair/lib/express/session.js' },
   { name: '@data-fair/processings-test-utils', message: 'Please use @data-fair/lib/processings/test-utils.js' },
   { name: 'rfdc', message: 'Please use @data-fair/lib/clone.js' },
-  { name: 'http-errors', message: 'Please use @data-fair/lib/http-errors.js' }
+  { name: 'http-errors', message: 'Please use @data-fair/lib/http-errors.js' },
+  { name: 'original-url', message: 'Please use @data-fair/lib/express/req-origin.js' }
 ]
 
 /** @type {import('eslint').ESLint.ConfigData} */
---
+++ src/express/req-origin.js
@@ -0,0 +1,34 @@
+// similar to https://www.npmjs.com/package/original-url without the path part
+// but it is important for us to have tight control over this as it can have some safety implications
+
+/**
+ * @param {import('express').Request} req
+ * @param {string} [pathPrefix ]
+ */
+export const reqOrigin = (req, pathPrefix = '') => {
+  // always use the host standard header sent by http client
+  // it is considered safe as it is used by the reverse proxy to perform the routing
+  const host = req.get('host')
+
+  // WARNING: in case of multiple layers of reverse proxies the host header might be changed
+  // in this case x-forwarded-host would be more suited, but to use it we have to be sure that is was defined
+  // by the reverse proxy, not by the client
+
+  // detect a situation where host and x-forwarded-host do not match
+  const forwardedHost = req.get('x-forwarded-host')
+  if (req.get('x-forwarded-host') && forwardedHost !== host) {
+    throw new Error(`host header "${host}" does not match x-forwarded-host header "${forwardedHost}"`)
+  }
+
+  // other infos are not as critical as host, we do our best with those almost standard headers to build a full origin
+  const proto = req.get('x-forwarded-proto') ?? req.get('x-forwarded-scheme') ?? req.get('x-scheme') ?? 'http'
+  const origin = `${proto}://${host}`
+  const port = req.get('x-forwarded-port')
+  if (port && !(port === '443' && proto === 'https') && !(port === '80' && proto === 'http')) {
+    return origin + ':' + port
+  } else {
+    return origin
+  }
+}
+
+export default reqOrigin
