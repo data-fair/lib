b7c97c13da9beb3af65996b8c5e9ea30b6eb0728
Alban Mouton <alban.mouton@gmail.com>
2024-09-12 11:27:40 +0200

    feat: work on vuetify eslint plugin

--- src/eslint/nuxt-recommended.js
+++ src/eslint/nuxt-recommended.js
@@ -1,7 +1,11 @@
 import { deprecatedModules } from './recommended.js'
 
+// this is a temporary submodule, see https://github.com/vuetifyjs/eslint-plugin-vuetify/issues/93#issuecomment-2296968464
+// @ts-ignore
+import pluginVuetify from '../../eslint-plugin-vuetify/src/configs/flat/base.js'
+
 const restrictedModules = deprecatedModules.concat([
-  { name: 'from vite-plugin-vuetify', message: 'Pleas use @data-fair/lib/nuxt.js' },
+  { name: 'vite-plugin-vuetify', message: 'Pleas use @data-fair/lib/nuxt.js' },
   { name: '@data-fair/lib/vue/session.js', message: 'Please use auto-import.' },
   { name: '@data-fair/lib/vue/reactive-search-params.js', message: 'Please use auto-import.' },
   { name: '@data-fair/lib/vue/reactive-search-params-global.js', message: 'Please use auto-import.' },
@@ -10,10 +14,10 @@ const restrictedModules = deprecatedModules.concat([
   { name: '@data-fair/lib/vuetify.js', message: 'Please use auto-import.' }
 ])
 
-/** @type {import('eslint').Linter.Config} */
-export default {
+/** @type {import('eslint').Linter.Config[]} */
+export default [{
   rules: {
     'no-restricted-imports': ['error', ...restrictedModules],
-    'no-restricted-modules': ['error', ...restrictedModules]
+    'no-restricted-modules': ['error', ...deprecatedModules.filter(dm => !dm.importNames)]
   }
-}
+}, pluginVuetify]
--- src/eslint/recommended.js
+++ src/eslint/recommended.js
@@ -5,15 +5,18 @@ export const deprecatedModules = [
   { name: '@data-fair/sd-vue', message: 'Please use @data-fair/lib/vue/session.js' },
   { name: '@data-fair/sd-express', message: 'Please use @data-fair/lib/express/session.js' },
   { name: '@data-fair/processings-test-utils', message: 'Please use @data-fair/lib/processings/test-utils.js' },
+  { name: '@data-fair/lib/express/async-handler.js', message: 'Please use native support of async functions with Express 5.' },
+  { name: '@data-fair/lib/express/index.js', importNames: ['asyncHandler'], message: 'Please use native support of async functions with Express 5.' },
+  // { patterns: ['*/async-handler.js', '*/async-wrap.js'], message: 'Please use native support of async functions with Express 5.' },
   { name: 'rfdc', message: 'Please use @data-fair/lib/clone.js' },
   { name: 'http-errors', message: 'Please use @data-fair/lib/http-errors.js' },
   { name: 'original-url', message: 'Please use @data-fair/lib/express/req-origin.js' }
 ]
 
-/** @type {import('eslint').Linter.Config} */
-export default {
+/** @type {import('eslint').Linter.Config[]} */
+export default [{
   rules: {
     'no-restricted-imports': ['error', ...deprecatedModules],
-    'no-restricted-modules': ['error', ...deprecatedModules]
+    'no-restricted-modules': ['error', ...deprecatedModules.filter(dm => !dm.importNames)]
   }
-}
+}]
