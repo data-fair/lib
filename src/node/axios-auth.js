0b70dd2a5756c79998d614caffcbd723e53f1ca8
Alban Mouton <alban.mouton@gmail.com>
2024-09-16 12:12:24 +0200

    chore: better config of axios headers

--- src/node/axios-auth.js
+++ src/node/axios-auth.js
@@ -31,8 +31,9 @@ export const axiosAuth = async (opts) => {
     await axiosInstance.get(callbackUrl, { maxRedirects: 0 })
   } catch (/** @type {any} */err) {
     if (err.status !== 302) throw err
-    axiosOpts.headers = axiosOpts.headers || {}
-    axiosOpts.headers.Cookie = err.headers['set-cookie'].map((/** @type {string} */s) => s.split(';')[0]).join(';')
+    axiosOpts.headers = axiosOpts.headers ?? {}
+    axiosOpts.headers.common = axiosOpts.headers.common ?? {}
+    axiosOpts.headers.common.Cookie = err.headers['set-cookie'].map((/** @type {string} */s) => s.split(';')[0]).join(';')
   }
   return axiosBuilder(axiosOpts)
 }
