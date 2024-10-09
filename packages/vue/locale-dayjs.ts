f890b724b5b830e9ab1621d2f72030b4c53662cf
Alban Mouton <alban.mouton@gmail.com>
2024-10-09 10:04:00 +0200

    chore: locale given to local-dayjs does not need to be reactive

--- packages/vue/locale-dayjs.ts
+++ packages/vue/locale-dayjs.ts
@@ -1,6 +1,6 @@
 import type { Dayjs, ConfigType } from 'dayjs'
-import type { Ref, App } from 'vue'
-import { inject, ref } from 'vue'
+import type { App } from 'vue'
+import { inject } from 'vue'
 import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 import 'dayjs/locale/en'
@@ -20,19 +20,19 @@ dayjs.extend(relativeTime)
 
 // main functionality, use through the createLocaleDayjs plugin and useLocaleDayjs composable
 // or as a global singleton through ./locale-dayjs-global.js
-export function getLocaleDayjs (_locale?: Ref<string>) {
-  const locale = _locale ?? ref('fr')
+export function getLocaleDayjs (locale?: string) {
+  locale = locale ?? 'fr'
   return {
     locale,
     dayjs: (date?: string | number | dayjs.Dayjs | Date | null | undefined) => {
-      return dayjs(date).locale(locale.value) as RelativeDayjs
+      return dayjs(date).locale(locale) as RelativeDayjs
     }
   }
 }
 
 // uses pattern for SSR friendly plugin/composable, cf https://antfu.me/posts/composable-vue-vueday-2021#shared-state-ssr-friendly
 export const localeDayjsKey = Symbol('localeDayjs')
-export function createLocaleDayjs (locale: Ref<string>) {
+export function createLocaleDayjs (locale?: string) {
   const localeDayjs = getLocaleDayjs(locale)
   return { install (app: App) { app.provide(localeDayjsKey, localeDayjs) } }
 }
