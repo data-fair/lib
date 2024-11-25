09ad3609836cf0d942d19c22f43b5861420e85f6
Alban Mouton <alban.mouton@gmail.com>
2024-11-25 11:10:15 +0100

    fix: better dayjs typing

--- packages/vue/locale-dayjs.ts
+++ packages/vue/locale-dayjs.ts
@@ -1,17 +1,17 @@
 // import type { Dayjs, ConfigType } from 'dayjs'
 import type { App } from 'vue'
 import { inject } from 'vue'
-import dayjs, { type Dayjs, ConfigType } from 'dayjs'
+import dayjs from 'dayjs'
 import 'dayjs/locale/fr'
 import 'dayjs/locale/en'
 import localizedFormat from 'dayjs/plugin/localizedFormat.js'
 import relativeTime from 'dayjs/plugin/relativeTime.js'
-import duration, { type Duration } from 'dayjs/plugin/duration.js'
+import duration from 'dayjs/plugin/duration.js'
 
 export type { ConfigType as DayjsConfigType } from 'dayjs'
 
 // for some reason our exported type did not inherit overloads by plugins
-type RelativeDayjs = Dayjs & {
+/* type RelativeDayjs = Omit<Dayjs, 'add' | 'subtract'> & {
   // copied from relativeTime.d.ts
   fromNow(withoutSuffix?: boolean): string
   from(compared: ConfigType, withoutSuffix?: boolean): string
@@ -20,6 +20,14 @@ type RelativeDayjs = Dayjs & {
   // copied from duration.d.ts
   add(duration: Duration): RelativeDayjs
   subtract(duration: Duration): RelativeDayjs
+} */
+declare module 'dayjs' {
+  interface Dayjs {
+    fromNow(withoutSuffix?: boolean): string
+    from(compared: ConfigType, withoutSuffix?: boolean): string
+    toNow(withoutSuffix?: boolean): string
+    to(compared: ConfigType, withoutSuffix?: boolean): string
+  }
 }
 
 dayjs.extend(localizedFormat)
@@ -36,7 +44,7 @@ export function getLocaleDayjs (locale?: string) {
       return dayjs.duration(dur).locale(locale)
     },
     dayjs: (date?: string | number | dayjs.Dayjs | Date | null | undefined) => {
-      return dayjs(date).locale(locale) as RelativeDayjs
+      return dayjs(date).locale(locale)
     }
   }
 }
