5a51d04a74b5ddc78b295781d8a9a2b0c13956f3
Alban Mouton <alban.mouton@gmail.com>
2024-11-25 10:43:05 +0100

    feat: add duration plugin to locale dayjs

--- packages/vue/locale-dayjs.ts
+++ packages/vue/locale-dayjs.ts
@@ -1,22 +1,30 @@
-import type { Dayjs, ConfigType } from 'dayjs'
+// import type { Dayjs, ConfigType } from 'dayjs'
 import type { App } from 'vue'
 import { inject } from 'vue'
-import dayjs from 'dayjs'
+import dayjs, { type Dayjs, ConfigType } from 'dayjs'
 import 'dayjs/locale/fr'
 import 'dayjs/locale/en'
 import localizedFormat from 'dayjs/plugin/localizedFormat.js'
 import relativeTime from 'dayjs/plugin/relativeTime.js'
+import duration, { type Duration } from 'dayjs/plugin/duration.js'
 
-// copied from relativeTime.d.ts because for some reason our exported type did not inherit this properly
+export type { ConfigType as DayjsConfigType } from 'dayjs'
+
+// for some reason our exported type did not inherit overloads by plugins
 type RelativeDayjs = Dayjs & {
+  // copied from relativeTime.d.ts
   fromNow(withoutSuffix?: boolean): string
   from(compared: ConfigType, withoutSuffix?: boolean): string
   toNow(withoutSuffix?: boolean): string
   to(compared: ConfigType, withoutSuffix?: boolean): string
+  // copied from duration.d.ts
+  add(duration: Duration): RelativeDayjs
+  subtract(duration: Duration): RelativeDayjs
 }
 
 dayjs.extend(localizedFormat)
 dayjs.extend(relativeTime)
+dayjs.extend(duration)
 
 // main functionality, use through the createLocaleDayjs plugin and useLocaleDayjs composable
 // or as a global singleton through ./locale-dayjs-global.js
@@ -24,6 +32,9 @@ export function getLocaleDayjs (locale?: string) {
   locale = locale ?? 'fr'
   return {
     locale,
+    duration: (dur: duration.DurationUnitsObjectType) => {
+      return dayjs.duration(dur).locale(locale)
+    },
     dayjs: (date?: string | number | dayjs.Dayjs | Date | null | undefined) => {
       return dayjs(date).locale(locale) as RelativeDayjs
     }
