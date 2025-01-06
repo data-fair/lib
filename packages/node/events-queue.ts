8c6b35046b5c99c197f38ebd08a28e810a452290
Alban Mouton <alban.mouton@gmail.com>
2025-01-06 14:50:34 +0100

    chore: add debug logs to events-queue

--- packages/node/events-queue.ts
+++ packages/node/events-queue.ts
@@ -2,6 +2,9 @@ import { type Event } from '@data-fair/lib-common-types/event/index.js'
 import { type Notification } from '@data-fair/lib-common-types/notification/index.js'
 import { internalError } from '@data-fair/lib-node/observer.js'
 import axios from './axios.js'
+import Debug from 'debug'
+
+const debug = Debug('events-queue')
 
 type EventsQueueOptions = {
   eventsUrl: string,
@@ -36,11 +39,13 @@ const drain = async () => {
   if (eventsQueue.length) {
     const events = eventsQueue
     eventsQueue = []
+    debug('drain events', events.length)
     await axios.post(options().eventsUrl + '/api/events', events, { headers: { 'x-secret-key': options().eventsSecret } })
   }
   if (notificationsQueue.length) {
     const notifications = notificationsQueue
     notificationsQueue = []
+    debug('drain notifications', notifications.length)
     for (const notification of notifications) {
       await axios.post(options().eventsUrl + '/api/notifications', notification, { headers: { 'x-secret-key': options().eventsSecret } })
     }
@@ -59,13 +64,17 @@ export const stop = async () => {
 }
 
 export const pushEvent = (event: Omit<Event, 'date'>) => {
+  options()
   if (stopped) throw new Error('events queue has been stopped');
   (event as Event).date = new Date().toISOString()
+  debug('pushEvent', event)
   eventsQueue.push(event as Event)
 }
 
 export const pushNotification = (notification: Omit<Notification, 'date'>) => {
+  options()
   if (stopped) throw new Error('notifications queue has been stopped');
   (notification as Notification).date = new Date().toISOString()
+  debug('pushNotification', notification)
   notificationsQueue.push(notification as Notification)
 }
