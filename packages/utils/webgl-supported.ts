d394c16ce0a6250961a43d1d16b8042fd13d83c0
Alban Mouton <alban.mouton@gmail.com>
2025-01-07 10:03:39 +0100

    feat: add util function to check webgl support

---
+++ packages/utils/webgl-supported.ts
@@ -0,0 +1,24 @@
+// cf https://maplibre.org/maplibre-gl-js/docs/examples/check-for-support/
+
+function isWebglSupported () {
+  if (window.WebGLRenderingContext) {
+    const canvas = document.createElement('canvas')
+    try {
+      // Note that { failIfMajorPerformanceCaveat: true } can be passed as a second argument
+      // to canvas.getContext(), causing the check to fail if hardware rendering is not available. See
+      // https://developer.mozilla.org/en-US/docs/Web/API/HTMLCanvasElement/getContext
+      // for more details.
+      const context = canvas.getContext('webgl2') || canvas.getContext('webgl')
+      if (context && typeof context.getParameter === 'function') {
+        return true
+      }
+    } catch (e) {
+      // WebGL is supported, but disabled
+    }
+    return false
+  }
+  // WebGL not supported
+  return false
+}
+
+export default isWebglSupported
