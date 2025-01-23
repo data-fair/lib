9f9d5754f1f25aa5eb1728bfd357cda887c9ce2e
Alban Mouton <alban.mouton@gmail.com>
2025-01-23 15:50:13 +0100

    chore: better stack trace in test spies

--- packages/node/test-spies.ts
+++ packages/node/test-spies.ts
@@ -1,4 +1,7 @@
 import { register } from 'node:module'
+import debugModule from 'debug'
+
+const debug = debugModule('test-spies')
 
 export function registerModuleHooks () {
   register('@data-fair/lib-node/test-module-hooks.js', { parentURL: import.meta.url })
@@ -13,6 +16,8 @@ export function waitFor <T> (eventName: string, timeout = 2000) {
   i++
   const id = i
 
+  debug(`Waiting for test spy ${eventName} (id=${id}, timeout: ${timeout}ms)`)
+
   // better stack traces
   // see https://github.com/axios/axios/issues/2387#issuecomment-652242713
   Error.stackTraceLimit = 0
@@ -23,9 +28,10 @@ export function waitFor <T> (eventName: string, timeout = 2000) {
   const p = new Promise<T>((resolve, reject) => {
     pendingPromises[eventName] = pendingPromises[eventName] ?? []
     pendingPromises[eventName].push({ resolve, id })
-    setTimeout(() => reject(new Error(`Timeout waiting for test spy ${eventName}`)), timeout)
+    setTimeout(() => reject(timeoutError), timeout)
   })
   p.finally(() => {
+    debug(`Cleaning up test spy ${eventName} (id=${id})`)
     pendingPromises[eventName] = pendingPromises[eventName].filter(p => p.id !== id)
     if (pendingPromises[eventName].length === 0) delete pendingPromises[eventName]
   })
@@ -33,7 +39,11 @@ export function waitFor <T> (eventName: string, timeout = 2000) {
 }
 
 export function emit (eventName: string, data: any) {
-  if (!pendingPromises[eventName]) return
+  debug(`Emitting test spy ${eventName} (${pendingPromises[eventName]?.length} listeners)`)
+  if (!pendingPromises[eventName]?.length) {
+    console.debug(`No listener for test spy ${eventName}`)
+    return
+  }
   for (const p of pendingPromises[eventName]) {
     p.resolve(data)
   }
