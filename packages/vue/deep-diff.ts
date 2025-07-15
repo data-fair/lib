edced781609f69091f03c137c49138a19a3e88fb
Alban Mouton <alban.mouton@gmail.com>
2025-07-15 20:30:00 +0200

    feat: work on deep-diff vue utils

---
+++ packages/vue/deep-diff.ts
@@ -0,0 +1,15 @@
+import { computed, watch, type ComputedGetter, type ComputedOptions, type WatchSource, type WatchCallback, type WatchOptions } from 'vue'
+import equal from 'fast-deep-equal'
+
+export const computedDeepDiff = <Type>(getter: ComputedGetter<Type>, options: ComputedOptions) => {
+  return computed<Type>((oldValue) => {
+    const newValue = getter()
+    return (oldValue !== undefined && equal(newValue, oldValue)) ? oldValue : newValue
+  }, options)
+}
+
+export const watchDeepDiff = (source: WatchSource, callback: WatchCallback, options: WatchOptions) => {
+  return watch(source, (newValue, oldValue, onCleanup) => {
+    if (!equal(newValue, oldValue)) callback(newValue, oldValue, onCleanup)
+  }, options)
+}
--- packages/vue/vite.ts
+++ packages/vue/vite.ts
@@ -13,5 +13,6 @@ export const autoImports = [
     '@data-fair/lib-vue/fetch.js': ['useFetch'],
     '@data-fair/lib-vue/ws.js': ['useWS'],
     '@data-fair/lib-vue/async-action.js': ['useAsyncAction'],
+    '@data-fair/lib-vue/deep-diff.js': ['computedDeepDiff', 'watchDeepDiff'],
   }
 ]
