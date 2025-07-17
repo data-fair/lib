047d20a294c14c0d92e353952d76b5f3327164c2
Alban Mouton <alban.mouton@gmail.com>
2025-07-17 16:23:06 +0200

    feat: add navigation-right component

---
+++ packages/vuetify/navigation-right.vue
@@ -0,0 +1,49 @@
+<template>
+  <v-navigation-drawer
+    v-if="display.lgAndUp.value"
+    class="navigation-right-drawer pl-2"
+    location="right"
+    permanent
+    floating
+    position="fixed"
+    color="surface"
+    style="padding-top: 60px; background-color: transparent;"
+  >
+    <slot />
+  </v-navigation-drawer>
+  <div
+    v-else
+    class="navigation-right-button mt-4"
+  >
+    <v-menu
+      location="bottom left"
+      :close-on-content-click="false"
+    >
+      <template #activator="{ props }">
+        <v-btn
+          v-bind="props"
+          size="small"
+          color="primary"
+          :icon="mdiDotsVertical"
+        />
+      </template>
+      <slot />
+    </v-menu>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { mdiDotsVertical } from '@mdi/js'
+import { useDisplay } from 'vuetify'
+
+const display = useDisplay()
+
+</script>
+
+<style>
+.navigation-right-button {
+  z-index: 1000;
+  position: fixed;
+  right: 20px;
+}
+</style>
--- packages/vuetify/vite.ts
+++ packages/vuetify/vite.ts
@@ -4,9 +4,19 @@ import { autoImports as vueAutoImports } from '@data-fair/lib-vue/vite.js'
 export const autoImports = [
   ...vueAutoImports,
   {
+    '@data-fair/lib-vuetify/date-match-filter.vue': [['default', 'dfDateMatchFilter']],
+    '@data-fair/lib-vuetify/date-range-picker.vue': [['default', 'dfDateRangePicker']],
+    '@data-fair/lib-vuetify/lang-switcher.vue': [['default', 'dfLangSwitcher']],
+    '@data-fair/lib-vuetify/navigation-right.vue': [['default', 'dfNavigationRight']],
+    '@data-fair/lib-vuetify/owner-avatar.vue': [['default', 'dfOwnerAvatar']],
+    '@data-fair/lib-vuetify/owner-pick.vue': [['default', 'dfOwnerPick']],
     '@data-fair/lib-vuetify/personal-menu.vue': [['default', 'dfPersonalMenu']],
+    '@data-fair/lib-vuetify/search-address.vue': [['default', 'dfSearchAddress']],
+    '@data-fair/lib-vuetify/theme-switcher.vue': [['default', 'dfThemeSwitcher']],
     '@data-fair/lib-vuetify/tutorial-alert.vue': [['default', 'dfTutorialAlert']],
-    '@data-fair/lib-vuetify/user-avatar.vue': [['default', 'dfUserAvatar']]
+    '@data-fair/lib-vuetify/ui-notif-alert.vue': [['default', 'dfUiNotifAlert']],
+    '@data-fair/lib-vuetify/ui-notif.vue': [['default', 'dfUiNotif']],
+    '@data-fair/lib-vuetify/ui-user-avatar.vue': [['default', 'dfUserAvatar']]
   }
 ]
 
