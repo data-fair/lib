a81a6727a98c8b9f8438d5410deae79cbdfcccd1
Alban Mouton <alban.mouton@gmail.com>
2025-09-09 13:58:33 +0200

    chore: improve eventPromise errors stack traces

--- packages/utils/event-promise.ts
+++ packages/utils/event-promise.ts
@@ -2,16 +2,34 @@ import type { EventEmitter } from 'node:events'
 
 type EventPromiseOptions = { timeout?: number }
 
+const { stackTraceLimit } = Error
+
+class EventPromiseContextError extends Error {
+  constructor () {
+    super()
+    this.name = 'EventPromiseContextError'
+  }
+}
+
 export function eventPromise <T> (emitter: EventEmitter, event: string, options?: EventPromiseOptions): Promise<T> {
   return new Promise((resolve, reject) => {
     const timeout = options?.timeout ?? 60000
+
+    // better stack traces
+    // see https://github.com/axios/axios/issues/2387#issuecomment-652242713
+    Error.stackTraceLimit = 0
+    const errorContext = new EventPromiseContextError()
+    Error.stackTraceLimit = stackTraceLimit
+    Error.captureStackTrace(errorContext, eventPromise)
 
     let timeoutRef: ReturnType<typeof setTimeout>
     if (timeout > 0) {
       timeoutRef = setTimeout(() => {
         emitter.off(event, callback)
         emitter.off('error', errorCallback)
-        reject(new Error(`Timeout of ${timeout}ms exceeded`))
+        const error = new Error(`Timeout of ${timeout}ms exceeded`)
+        error.stack += '\n' + errorContext.stack
+        reject(error)
       }, timeout)
     }
 
