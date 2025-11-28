425c2328250a400f7cb88aa7062eea7986e3e1e9
Alban Mouton <alban.mouton@gmail.com>
2025-11-28 11:39:00 +0100

    chore: add a vite plugin to force closing hanging builds

--- packages/vue/vite.ts
+++ packages/vue/vite.ts
@@ -16,3 +16,26 @@ export const autoImports = [
     '@data-fair/lib-vue/deep-diff.js': ['computedDeepDiff', 'watchDeepDiff'],
   }
 ]
+
+// cf https://stackoverflow.com/questions/75839993/vite-build-hangs-forever/76920975#76920975
+export function ClosePlugin () {
+  return {
+    name: 'ClosePlugin', // required, will show up in warnings and errors
+
+    // use this to catch errors when building
+    buildEnd (error: any) {
+      if (error) {
+        console.error(error)
+        process.exit(1)
+      } else {
+        console.log('Build ended')
+      }
+    },
+
+    // use this to catch the end of a build without errors
+    closeBundle () {
+      console.log('Bundle closed')
+      process.exit(0)
+    },
+  }
+}
