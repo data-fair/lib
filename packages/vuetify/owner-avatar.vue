5135ff6eaa7d567a63ab0079b2de75bf9cb6dbd5
Baptiste GUERNY <baptiste.guerny@koumoul.com>
2026-01-12 10:56:59 +0100

    feat: improve owner-avatar component (#18)

--- packages/vuetify/owner-avatar.vue
+++ packages/vuetify/owner-avatar.vue
@@ -1,36 +1,57 @@
 <template>
-  <v-tooltip location="top">
-    <template #activator="{ props }">
+  <v-tooltip
+    v-if="showTooltip"
+    location="top"
+  >
+    <template #activator="{ props: tooltipProps }">
       <span
-        v-bind="props"
+        v-bind="tooltipProps"
         class="text-body-2"
       >
-        <v-avatar :size="28" :image="avatarUrl" />
+        <v-avatar
+          :size="size"
+          :image="avatarUrl"
+          class="bg-transparent"
+        />
       </span>
     </template>
     {{ label }}
   </v-tooltip>
+  <span v-else>
+    <v-avatar
+      :size="size"
+      :image="avatarUrl"
+      class="bg-transparent"
+    />
+  </span>
 </template>
 
 <script setup lang="ts">
 import { computed } from 'vue'
 
-const ownerProps = defineProps({
-  owner: {
-    type: Object,
-    default: null
-  }
+const props = withDefaults(defineProps<{
+  owner: Record<string, any>
+  size?: number
+  /** If true, a tooltip with the owner's name and department will be shown (default: true) */
+  showTooltip?: boolean
+  /** If true, the owner's name will be omitted from the tooltip when the department is present */
+  omitOwnerName?: boolean
+}>(), {
+  size: 28,
+  showTooltip: true,
+  omitOwnerName: false
 })
 
 const avatarUrl = computed(() => {
-  if (ownerProps.owner.department) return `/simple-directory/api/avatars/${ownerProps.owner.type}/${ownerProps.owner.id}/${ownerProps.owner.department}/avatar.png`
-  else return `/simple-directory/api/avatars/${ownerProps.owner.type}/${ownerProps.owner.id}/avatar.png`
+  if (props.owner.department) return `/simple-directory/api/avatars/${props.owner.type}/${props.owner.id}/${props.owner.department}/avatar.png`
+  else return `/simple-directory/api/avatars/${props.owner.type}/${props.owner.id}/avatar.png`
 })
 
 const label = computed(() => {
-  let label = ownerProps.owner.name
-  if (ownerProps.owner.department) label += ' - ' + (ownerProps.owner.departmentName || ownerProps.owner.department)
-  if (ownerProps.owner.role) label += ` (${ownerProps.owner.role})`
+  let label = ''
+  if (!props.omitOwnerName || !props.owner.department) label += props.owner.name
+  if (props.owner.department) label += ' - ' + (props.owner.departmentName || props.owner.department)
+  if (props.owner.role) label += ` (${props.owner.role})`
   return label
 })
 </script>
