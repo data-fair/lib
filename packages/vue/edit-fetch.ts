93f0263d30042f769617e48b8b37008f10c03c78
Alban Mouton <alban.mouton@gmail.com>
2026-02-09 09:28:06 +0100

    chore: some changes to useEditFetch

--- packages/vue/edit-fetch.ts
+++ packages/vue/edit-fetch.ts
@@ -13,28 +13,32 @@ type OptionalUrl = string | null | undefined
 
 export function useEditFetch<T extends Record<string, any>> (url: OptionalUrl | Ref<OptionalUrl> | (() => OptionalUrl), options: UseEditFetchOptions = {}) {
   const fetch = useFetch<T>(url, options)
+  const serverData = ref<T | null>(null)
   const data = ref<T | null>(null)
   watch(fetch.data, () => {
     // TODO: check for local changes before overwriting ?
+    serverData.value = fetch.data.value
     data.value = fetch.data.value
   })
   const save = useAsyncAction(async () => {
-    if (!data.value || !fetch.data.value) throw new Error('cannot save data that has not been fetched yet')
+    if (!data.value || !serverData.value || !fetch.data.value) throw new Error('cannot save data that has not been fetched yet')
     if (options.patch) {
       const patch: any = {}
       for (const key of Object.keys(data.value)) {
-        if (!equal(data.value[key], fetch.data.value[key])) patch[key] = data.value[key]
+        if (!equal(data.value[key], serverData.value[key])) patch[key] = data.value[key]
       }
       if (!Object.keys(patch).length) return
-      await ofetch(fetch.fullUrl.value!, { method: 'PATCH', body: patch })
+      serverData.value = await ofetch<T>(fetch.fullUrl.value!, { method: 'PATCH', body: patch })
     } else {
       // TODO: add if-unmodified-since header ?
-      await ofetch(fetch.fullUrl.value!, { method: 'PUT', body: data.value })
+      serverData.value = await ofetch<T>(fetch.fullUrl.value!, { method: 'PUT', body: data.value })
     }
+    serverData.value = data.value
   }, options.saveOptions)
   return {
     fetch,
     data,
+    serverData,
     save
   }
 }
