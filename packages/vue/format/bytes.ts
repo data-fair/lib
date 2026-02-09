caf84c345cc0aa260ab44772919c61506d7a24f2
Alban Mouton <alban.mouton@gmail.com>
2026-02-09 09:56:18 +0100

    fix: broken export of format-bytes

--- packages/vue/format/bytes.ts
+++ packages/vue/format/bytes.ts
@@ -1,3 +1,5 @@
 // re-export for retro-compatiblity
+import { formatBytes } from '@data-fair/lib-utils/format/bytes.js'
 
 export * from '@data-fair/lib-utils/format/bytes.js'
+export default formatBytes
